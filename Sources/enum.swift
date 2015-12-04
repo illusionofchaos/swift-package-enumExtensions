fc4ae67b6c9059535733d165dc6c310029af4fd4
Denis Tokarev <bizzzar@gmail.com>
2015-12-04 23:51:02 +0300

    make all declarations public

--- Sources/enum.swift
+++ Sources/enum.swift
@@ -1,24 +1,24 @@
 
-extension RawRepresentable where RawValue: Strideable {
-    func increment() -> Self {
+public extension RawRepresentable where RawValue: Strideable {
+    public func increment() -> Self {
         return Self(rawValue: rawValue + 1) ?? self
     }
-    func decrement() -> Self {
+    public func decrement() -> Self {
         return Self(rawValue: rawValue - 1) ?? self
     }
-    mutating func incrementInPlace() {
+    public mutating func incrementInPlace() {
         self = increment()
     }
-    mutating func decrementInPlace() {
+    public mutating func decrementInPlace() {
         self = decrement()
     }
 }
 
-extension RawRepresentable where RawValue:IntegerType {
+public extension RawRepresentable where RawValue:IntegerType {
     /**
      - Attention: the result will be correct only if all raw values are sequential and start from zero
      */
-    static var count: Int {
+    public static var count: Int {
         var i: RawValue = 0
         while let _ = Self(rawValue:i) {
             i++
@@ -28,7 +28,7 @@ extension RawRepresentable where RawValue:IntegerType {
     /**
      - Attention: the result will be correct only if all raw values are sequential and start from zero
      */
-    static var allValues: [Self] {
+    public static var allValues: [Self] {
         var values = [Self]()
         var i: RawValue = 0
         while let value = Self(rawValue:i) {
@@ -41,50 +41,50 @@ extension RawRepresentable where RawValue:IntegerType {
 
 
 
-func == <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
+public func == <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
     return lhs.rawValue == rhs
 }
 
-func != <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
+public func != <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
     return lhs.rawValue != rhs
 }
 
-func > <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
+public func > <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
     return lhs.rawValue > rhs
 }
 
-func < <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
+public func < <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
     return lhs.rawValue < rhs
 }
 
-func >= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
+public func >= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
     return lhs.rawValue >= rhs
 }
 
-func <= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
+public func <= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:A,rhs:B) -> Bool {
     return lhs.rawValue <= rhs
 }
 
-func == <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
+public func == <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
     return lhs == rhs.rawValue
 }
 
-func != <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
+public func != <A:RawRepresentable,B:Equatable where A.RawValue:Equatable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
     return lhs != rhs.rawValue
 }
 
-func > <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
+public func > <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
     return lhs > rhs.rawValue
 }
 
-func < <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
+public func < <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
     return lhs < rhs.rawValue
 }
 
-func >= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
+public func >= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
     return lhs >= rhs.rawValue
 }
 
-func <= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
+public func <= <A:RawRepresentable,B:Comparable where A.RawValue:Comparable, A.RawValue==B>(lhs:B,rhs:A) -> Bool {
     return lhs <= rhs.rawValue
 }
